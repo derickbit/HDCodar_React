8e2977d8fb718a9fe813d459ecee1e43ada65129
Derick Bitencourte da Silva <derickbit7@gmail.com>
2024-11-21 22:27:55 -0300

    Aula 10 - Events

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
 import "./App.css";
-import HelloWorld from "./components/HelloWorld";
-import SayMyName from "./components/SayMyName";
-import Pessoa from "./components/Pessoa";
-import Frase from "./components/Frase";
-import List from "./components/List";
+import Evento from "./components/Evento";
+import Form from "./components/Form";
 
 function App() {
-  const name = "Heisenberg";
-
-  const newName = name.toUpperCase();
-
-  function sum(a, b) {
-    return a + b;
-  }
-
-  const url = "https://via.placeholder.com/150";
-
   return (
     <div className="App">
-      <h1>OLÁ</h1>
-      <Frase />
-      <p>Olá, {newName}</p>
-      <p>Soma: {sum(1, 2)}</p>
-      <img src={url} alt="minha imagem"></img>
-      <SayMyName nome="Heisenberg" />
-      <HelloWorld />
-      <Pessoa
-        nome="Dérick"
-        idade="28"
-        profissao="vagabundo"
-        foto="https://via.placeholder.com/50"
-      />
-      <List />
+      <h1>Testando Eventos</h1>
+      <Evento numero="1" />
+      <Evento numero="2" />
+      <Form />
     </div>
   );
 }
