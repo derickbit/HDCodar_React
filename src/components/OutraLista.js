0b180dfea2920b6e3c03239b2fc61455bacd80fd
Derick Bitencourte da Silva <derickbit7@gmail.com>
2024-11-24 22:59:17 -0300

    aula 12 e 13 - renderização condicional e listas

---
+++ src/components/OutraLista.js
@@ -0,0 +1,14 @@
+function OutraLista({ itens }) {
+  return (
+    <>
+      <h3>Lista de coisas boas:</h3>
+      {itens.length > 0 ? (
+        itens.map((itens, index) => <p key={index}>{itens}</p>)
+      ) : (
+        <p>Não há itens na lista!</p>
+      )}
+    </>
+  );
+}
+
+export default OutraLista;
